3dc43eada3d09a9c8cb910ad6a6369b758781e28
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2023-06-11 19:23:25 -0300

    refact: change config for user .env test and production db connection

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,22 @@
 import { PrismaClient } from '@prisma/client';
+import { config } from 'dotenv';
+import { join } from 'path';
+import { ok } from 'assert';
 
-const prisma = new PrismaClient();
+const env = process.env.NODE_ENV || 'test'
+ok(env === 'prod' || env === 'test', 'a env é inválida, ou test ou prod')
+
+const configPath = join(__dirname, '../', `.env.${env}`)
+config({
+  path: configPath
+})
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    }
+  }
+});
 
 export default prisma;
